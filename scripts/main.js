124ac78bdaa3b3added987667f3ac5efcfd9b57b
pranshuchittora <pranshuchittora17@gmail.com>
2018-10-09 16:56:58 +0530

    Added Max Size & stack stats

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 const stackContElm = document.getElementById('stack-cont')
 const pushInputElm = document.getElementById('push-value-input')
 const peekElm = document.getElementById('peek-value')
-
+const stackSizeElm = document.getElementById('stack-size')
+const stackStatElm = document.getElementById('stack-status')
 let stack = []
+let max_stack_size = 5;
 
 const clear_stack_dom = () => {
     while (stackContElm.firstChild) {
@@ -11,6 +13,7 @@ const clear_stack_dom = () => {
 }
 const re_render_stack = () => {
     clear_stack_dom()
+    stackStatus()
     stack.forEach(elm => {
         console.log(elm)
         let new_div = document.createElement('div')
@@ -29,16 +32,30 @@ const increment_push_value = () => {
 const decrement_push_value = () => {
     pushInputElm.value--
 }
+// Overflow
+
+const overflow = () => {
+    if (stack.length >= max_stack_size) {
+        alert("Stack Overflow");
+        return true
+    } else
+        return false
+}
 
 // Add to last -> enqueue
 // Remove First -> dequeue
 
 const push_to_stack = () => {
-    console.log(pushInputElm.value)
-    stack.push(pushInputElm.value)
 
-    peek_to_stack()
-    re_render_stack()
+    if (overflow()) {
+        return
+    } else {
+        console.log(pushInputElm.value)
+        stack.push(pushInputElm.value)
+
+        peek_to_stack()
+        re_render_stack()
+    }
 }
 const pop_from_stack = () => {
     stack.pop()
@@ -54,4 +71,21 @@ const peek_to_stack = () => {
     }
     peekElm.innerHTML = peekVal
     console.log('Peek -> ' + peekVal)
+}
+
+const stackStatus = () => {
+    const current_size = stack.length;
+    stackStatElm.innerHTML = current_size + "/" + max_stack_size
+}
+
+
+const changeStackSize = () => {
+    const new_size = stackSizeElm.value;
+    if (new_size < stack.length) {
+        alert("Invalid size declaration");
+        stackSizeElm.value = max_stack_size;
+    } else {
+        max_stack_size = new_size
+    }
+
 }
